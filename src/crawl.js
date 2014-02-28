5b2865dc3da5105ab546dd6bff94ea5e881185b5
Daniel McLaren <daniel_mclaren@acl.com>
2014-02-28 15:44:40 -0800

    Got our hooks into angular!

--- src/crawl.js
+++ src/crawl.js
@@ -9,6 +9,64 @@ var path = require("path"),
   modules = {},
   injectables = {};
 
+/// BEGIN stolen from Angular.
+
+var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
+var FN_ARG_SPLIT = /,/;
+var FN_ARG = /^\s*(_?)(\S+?)\1\s*$/;
+var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+
+function annotate(fn) {
+  var $inject,
+      fnText,
+      argDecl,
+      last;
+
+  if (typeof fn == 'function') {
+    if (!($inject = fn.$inject)) {
+      $inject = [];
+      fnText = fn.toString().replace(STRIP_COMMENTS, '');
+      argDecl = fnText.match(FN_ARGS);
+      forEach(argDecl[1].split(FN_ARG_SPLIT), function(arg){
+        arg.replace(FN_ARG, function(all, underscore, name){
+          $inject.push(name);
+        });
+      });
+      fn.$inject = $inject;
+    }
+  } else if (isArray(fn)) {
+    last = fn.length - 1;
+    assertArgFn(fn[last], 'fn');
+    $inject = fn.slice(0, last);
+  } else {
+    assertArgFn(fn, 'fn', true);
+  }
+  return $inject;
+}
+
+function isArray(value) {
+  return toString.apply(value) == '[object Array]';
+}
+
+function assertArg(arg, name, reason) {
+  if (!arg) {
+    throw new Error("Argument '" + (name || '?') + "' is " + (reason || "required"));
+  }
+  return arg;
+}
+
+function assertArgFn(arg, name, acceptArrayAnnotation) {
+  if (acceptArrayAnnotation && isArray(arg)) {
+      arg = arg[arg.length - 1];
+  }
+
+  assertArg(isFunction(arg), name, 'not a function, got ' +
+      (arg && typeof arg == 'object' ? arg.constructor.name || 'Object' : typeof arg));
+  return arg;
+}
+
+function isFunction(value){return typeof value == 'function';}
+
 function addModule(name) {
   nodes.push({name: name, type: 'module'});
   return modules[name] = nodes.length;
@@ -19,9 +77,36 @@ function addInjectable(name) {
   return injectables[name] = nodes.length;
 }
 
+function forEach(obj, iterator, context) {
+  var key;
+  if (obj) {
+    if (isFunction(obj)){
+      for (key in obj) {
+        if (key != 'prototype' && key != 'length' && key != 'name' && obj.hasOwnProperty(key)) {
+          iterator.call(context, obj[key], key);
+        }
+      }
+    } else if (obj.forEach && obj.forEach !== forEach) {
+      obj.forEach(iterator, context);
+    } else if (isArrayLike(obj)) {
+      for (key = 0; key < obj.length; key++)
+        iterator.call(context, obj[key], key);
+    } else {
+      for (key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          iterator.call(context, obj[key], key);
+        }
+      }
+    }
+  }
+  return obj;
+}
+
+/// END stolen from Angular.
+
 // Mess with angular.
 angular.module = (function() {
-  var module = angular.module;
+  var moduleFn = angular.module;
   return function(moduleName, deps) {
     var moduleIndex = addModule(moduleName);
     deps.forEach(function(d) {
@@ -31,7 +116,65 @@ angular.module = (function() {
         type: 'dependency'
       });
     });
-    return module.apply(angular, arguments);
+
+    // Mess with the module object it returns, too!
+    var moduleObj = moduleFn.apply(angular, arguments);
+
+    moduleObj.controller = (function() {
+      var controllerFn = moduleObj.controller;
+      return function(name, fn) {
+        var injectableIndex = injectables[name] || addInjectable(name);
+
+        var injections = annotate(fn);
+        injections.forEach(function(d) {
+          edges.push({
+            source: injectableIndex,
+            target: injectables[d] || addInjectable(d),
+            type: 'injection'
+          });
+        });
+
+        return controllerFn.apply(moduleObj, arguments);
+      };
+    })();
+
+    moduleObj.factory = (function() {
+      var factoryFn = moduleObj.factory;
+      return function(name, fn) {
+        var injectableIndex = injectables[name] || addInjectable(name);
+
+        var injections = annotate(fn);
+        injections.forEach(function(d) {
+          edges.push({
+            source: injectableIndex,
+            target: injectables[d] || addInjectable(d),
+            type: 'injection'
+          });
+        });
+
+        return factoryFn.apply(moduleObj, arguments);
+      };
+    })();
+
+    moduleObj.service = (function() {
+      var serviceFn = moduleObj.service;
+      return function(name, fn) {
+        var injectableIndex = injectables[name] || addInjectable(name);
+
+        var injections = annotate(fn);
+        injections.forEach(function(d) {
+          edges.push({
+            source: injectableIndex,
+            target: injectables[d] || addInjectable(d),
+            type: 'injection'
+          });
+        });
+
+        return serviceFn.apply(moduleObj, arguments);
+      };
+    })();
+
+    return moduleObj;
   };
 })();
 
@@ -47,6 +190,8 @@ var p = path.join(__dirname, "../data/ng-boilerplate-0.3.1.js");
 var data = fs.readFileSync(p);
 var script = vm.createScript(data, p);
 script.runInNewContext(sandbox);
+
+fs.writeFileSync(path.join(__dirname, "../data/ng-boilerplate.json"), 'var miserables = ' + JSON.stringify({nodes: nodes, links: edges}, null, '  '));
 
 console.log(edges.map(function(d) { return {source: nodes[d.source].name, target: nodes[d.target].name}; }));
 
